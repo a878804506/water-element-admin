1cd6424ba44d8c949ccf55bf6f66ac4c70cd421f
252547545 <LIUfeng520>
2019-09-25 15:55:08 +0800

    跨域配置，开发配置完善

--- vue.config.js
+++ vue.config.js
@@ -28,23 +28,12 @@ module.exports = {
   productionSourceMap: false,
   devServer: {
     port: port,
-    open: true,
+    // 开发模式：是否自动打开浏览器群
+    open: false,
     overlay: {
       warnings: false,
       errors: true
-    },
-    proxy: {
-      // change xxx-api/login => mock/login
-      // detail: https://cli.vuejs.org/config/#devserver-proxy
-      [process.env.VUE_APP_BASE_API]: {
-        target: `http://127.0.0.1:${port}/mock`,
-        changeOrigin: true,
-        pathRewrite: {
-          ['^' + process.env.VUE_APP_BASE_API]: ''
-        }
-      }
-    },
-    after: require('./mock/mock-server.js')
+    }
   },
   configureWebpack: {
     // provide the app's title in webpack's name field, so that
